1ac7d33c27ff6b08e09841fb6cf10c8c78a62c03
Connor <noxor0@users.noreply.github.com>
2016-07-10 09:22:26 -0700

    !Editor +Guistuff

--- OpenDialog.cs
+++ OpenDialog.cs
@@ -1,47 +1,70 @@
 ﻿using UnityEngine;
-using UnityEditor;
 using System.Collections;
 using System.IO;
 using System.Text;
 
 //Opens dialog for the observer
-public class OpenDialog : MonoBehaviour {
+public class OpenDialog : MonoBehaviour{
     //Holds all the questions and available responses
     static string[] myIndex;
     static StringBuilder myStringBuilder = new StringBuilder();
+    private static Rect myWindowRect = new Rect((Screen.width - 200) / 2,
+                                (Screen.height - 300) / 2, 300, 300);
+    private static string myCurrQuestion;
+    private static string myCurrOption1;
+    private static string myCurrOption2;
+    private static string selectedOption;
+    private static bool isVisible = false;
 	// Use this for initialization
 	void Start () {
         //"questions.txt" will most likely need to be changed to \\Assets\\questions.txt
         // or you can leave it in root folder.
         StreamReader sr = File.OpenText("questions.txt");
-        myIndex = sr.Split(',');
+        string line;
+        while ((line = sr.ReadLine()) != null) {
+             myIndex = line.Split(',');
         }
+    }
+
+    void OnGUI() {
+        if (isVisible) {
+            myWindowRect = GUI.Window(0, myWindowRect, DialogWindow, "What Should I do?");
+        }
+    }
+
+    void DialogWindow(int WindowID) {
+        GUI.Label(new Rect(5, 20, myWindowRect.width, 20), myCurrQuestion);
+        if (GUI.Button(new Rect(5, 270, (myWindowRect.width - 10) / 2, 30), myCurrOption1)) {
+            selectedOption = myCurrOption1;
+            isVisible = false;
+        }
+        if (GUI.Button(new Rect(155, 270, (myWindowRect.width - 10) / 2, 30), myCurrOption2)) {
+            selectedOption = myCurrOption2;
+            isVisible = false;
+        }
+        myStringBuilder.Append(selectedOption);
+    }
+
     //if you want to have a interlocking story (where nodes point back to each other)
     // then you gotta send do: the question - 1
     // so it goes back to the previous question (you do the wrong response twice, but
     // i dont want to make that better
     // @param the 'wave/round' of question the user is currently on
-	static string PromptUser(int theQuestionLevel) {
+	public static void PromptUser(int theQuestionLevel) {
         //the value will change depending on the selection. 
-        string currQuestion = myIndex[(theQuestionLevel * 3) + 0];
+        myCurrQuestion = myIndex[(theQuestionLevel * 3) + 0];
         //good response is going to return true
-        string currOption1 = myIndex[(theQuestionLevel * 3) + 1];
+        myCurrOption1 = myIndex[(theQuestionLevel * 3) + 1];
         //bad response is going to return false
-        string currOption2 = myIndex[(theQuestionLevel * 3) + 2];
-        //Display shit
-        bool isCorrect = EditorUtility.DisplayDialog("Make the choice!", currQuestion, currOption1, currOption2);
-        //translate back to text
-        string returnStr;
-        if (isCorrect) {
-            returnStr = currOption1;
-        } else {
-            returnStr = currOption2;
-        }
-        myStringBuilder.Append(returnStr + '\n');
-        return returnStr;
+        myCurrOption2 = myIndex[(theQuestionLevel * 3) + 2];
+        isVisible = true;
     }
+    public static string getLastChoice() {
+        return selectedOption;
+    }
+
     //Show the path that the student took (log)
-    static string getReview() {
+    public static string getReview() {
         return myStringBuilder.ToString();
     }
 }
